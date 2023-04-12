9485906cf341f77e2f24d11b62366d8f1ef2f913
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-04-12 15:56:18 -0300

    feat: consumo de restaurantes comidas y categorias

--- src/pages/app/profile.jsx
+++ src/pages/app/profile.jsx
@@ -12,3 +12,91 @@ export default function Profile() {
 	);
 }
 
+/* import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import Collapsible from "react-native-collapsible";
+import { ArrowSvg, BookSvg, EditSvg, HeartSvg, LocationSvg } from "../svgs/Svgs";
+import { StyleSheet } from "react-native";
+
+export default function Accordion({ title,iconType,userAddresses,userDara, userFavorites}) {
+  const [isCollapsed, setIsCollapsed] = React.useState(true);
+  const [collapsibleContent, setCollapsibleContent] = React.useState('address');
+
+  const toggleAccordion = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  icons props
+  const icon = {
+    heart: <HeartSvg height={16} width={16} fill="#00869F" />,
+    book: <BookSvg height={16} width={16} fill="#00869F" />,
+    location: <LocationSvg height={16} width={16} fill="#00869F" />,
+  }
+
+
+  return (
+    <View style={{ zIndex: 1 }}>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          height: 60,
+          paddingHorizontal: 16,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+        onPress={toggleAccordion}
+      >
+        {icon[iconType]}
+        <Text style={{ color: "#4D4D4D", width: "80%" }}>{title}</Text>
+        <ArrowSvg height={16} width={16} fill="#00869F" rotation={0}/>
+      </TouchableOpacity>
+
+
+      <Collapsible collapsed={isCollapsed} style={styles.accordionBody}>
+        {collapsibleContent === 'address' ? (
+          <View style={styles.cardContent}>
+          <Text>direcciones</Text>
+          <EditSvg height={16} width={16} fill="#00869F" />
+        </View>
+        ) : collapsibleContent === 'data' ? (
+          <View style={styles.cardContent}>
+          <Text>datos</Text>
+          <EditSvg height={16} width={16} fill="#00869F" />
+        </View>
+        ) : collapsibleContent === 'favorites' ? (
+          <View style={styles.cardContent}>
+          <Text>favoritos</Text>
+          <EditSvg height={16} width={16} fill="#00869F" />
+        </View>
+        ) : null}
+        
+      </Collapsible>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContent: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    backgroundColor: "green",
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.70,
+    shadowRadius: 3,
+    elevation: 5,
+    height:35
+  },
+  accordionBody: {
+    padding: 16,
+  },
+}); */
