b001ed989ab10a7b7152b7f40a563c821d9a74d6
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-04-14 17:19:22 -0300

    Add files via upload

---
+++ Api/index.js
@@ -0,0 +1,28 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const { preloadUsers, preloadRest, preloadFood, preloadOrders } = require('./src/routes/utils.js')
+
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    preloadUsers()
+    setTimeout(()=>{
+      preloadRest()
+    },1000)
+    setTimeout(()=>{
+      preloadFood()
+    },2000)
+    setTimeout(()=>{
+      preloadOrders()
+    },3000)
+    
+
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
+
+
+setTimeout(()=>{
+
+},1000)
