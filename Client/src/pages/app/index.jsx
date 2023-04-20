3e2a4e9561a5bd38babfd3eb20314be6942e1e67
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-04-20 03:43:40 -0300

    Update index.jsx

--- Client/src/pages/app/index.jsx
+++ Client/src/pages/app/index.jsx
@@ -6,7 +6,7 @@ import InputSearchFood from "../../components/cards/InputSearchFood";
 import axios from "axios";
 
 function Home() {
-  const ip = "192.168.56.1";
+  const ip = "localhost";
   const [isLoading, setIsLoading] = React.useState(true);
   const [dataRestaurants, setDataRestaurants] = React.useState([]);
   const [dataFood, setDataFood] = React.useState([]);
